2218ae08dbe68ceb523456067fc1f0a081fba0c3
methiathibault <78618802+methiathibault@users.noreply.github.com>
2021-09-29 00:10:59 +0200

    Add files via upload

---
+++ compteur de mort.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+def compteurdemort(event):
+    f = open('D:\Documents\Phoenyx\mort.txt','r+')
+    global mort
+    t=event.keysym
+    if t == "plus":
+        mort = mort +1
+        s=str(mort)
+        f.write(s)
+    elif t == "minus":
+        mort-=1
+        s=str(mort)
+        f.write(s)
+    elif t == "0":
+        mort=0
+        s=str(mort)
+        f.write(s)
+root = Tk()
+mort = 0
+root.bind("<Key>", compteurdemort)
+root.mainloop()
